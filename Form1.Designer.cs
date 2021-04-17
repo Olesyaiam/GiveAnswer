3822398dc4d03ae517a4a9fb072b0b276099d2ce
Olesyaiam <olesyaiam13@mail.ru>
2021-04-17 17:42:40 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace GiveAnswer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonAnswer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.buttonAnswer);
+            this.splitContainer1.Size = new System.Drawing.Size(410, 149);
+            this.splitContainer1.SplitterDistance = 79;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(406, 75);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // buttonAnswer
+            // 
+            this.buttonAnswer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAnswer.Location = new System.Drawing.Point(0, 0);
+            this.buttonAnswer.Name = "buttonAnswer";
+            this.buttonAnswer.Size = new System.Drawing.Size(406, 62);
+            this.buttonAnswer.TabIndex = 0;
+            this.buttonAnswer.Text = "Дай мне ответ";
+            this.buttonAnswer.UseVisualStyleBackColor = true;
+            this.buttonAnswer.Click += new System.EventHandler(this.buttonAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 149);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonAnswer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiveAnswer
+{
+    public partial class Form1 : Form
+    {
+        private const string AppName = "Give answer";
+        private readonly string ConfigPath = $"{Environment.CurrentDirectory}\\Config.json";
+        private string[] _predictions;
+        private Random _random = new Random();
+
+        public Form1() // в конструкторе не может работать метод Close(), т.к. форма еще не загрузилась
+        {
+            InitializeComponent();
+            Text = AppName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // выполняется метод когда форма полностью загрузилась на экран
+        {
+            try
+            {
+                string data = File.ReadAllText(ConfigPath);
+
+                // в поле сохраняем результат конвертации в массив строк из переменной data
+                _predictions = JsonConvert.DeserializeObject<string[]>(data);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Файла {ConfigPath} не существует");
+                Close();
+            }
+        }
+
+        private async void buttonAnswer_Click(object sender, EventArgs e)
+        {
+            // делаем false чтобы не было возможности несколько раз запускать
+            buttonAnswer.Enabled = false;
+
+            // для избежания фризов блокировки юзер интерфейса, сделаем цикл фор в отдельном потоке
+            // реализуем с помощью Task (Одна задача может запускать другую - вложенную задачу.
+            // При этом эти задачи выполняются независимо друг от друга)
+            await Task.Run(() => // с помощью await ждем 
+            {
+                // Заполнение прогрессбара от 0 до 100
+                for (int i = 1; i <= 120; i++)
+                {
+                    // метод Invoke полезен в случаях, когда необходимо работать с контролом
+                    // из других потоков
+                    Invoke(new Action(() =>
+                    {
+                        double procent = Math.Round((i - 10) / 1.1);
+
+                        if (i < 10)
+                        {
+                            procent = 0;
+                        }
+
+                        Text = $"{procent}%";
+
+                        progressBar1.Value = i >= 100 ? 100 : i; ;
+
+                    }));
+                    // обращаемся к классу thread вызываем метод sleep 
+
+                    Thread.Sleep(20);
+                }
+
+                Thread.Sleep(100);
+
+                Invoke(new Action(() =>
+                {
+                    // генерируем число от 0 до размера массива (кол-ва предсказаний)
+                    int index = _random.Next(0, _predictions.Length);
+
+
+                    MessageBox.Show(_predictions[index]);
+                    buttonAnswer.Enabled = true;
+                }));
+
+            });
+
+
+
+            progressBar1.Value = 0;
+            Text = AppName;
+        }
+
+
+    }
+}
